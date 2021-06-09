d1ddd57a475fdedd422326bfaed993686aa4d56c
Lacey <lacey.griffith@g.austincc.edu>
2021-06-09 13:06:52 -0500

    add functionality for replies on front end

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -146,6 +146,26 @@ function handleNewReplySubmit(event) {
 
   const formData = { writtenBy, replyBody };
 
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(res => {
+    if(!res.ok){
+      throw new Error('Something went wrong!')
+    }
+    res.json();
+  })
+  .then(commentRes => {
+    location.reload();
+  })
+  .catch(err => {
+    console.log(err)
+  })
 }
 
 $backBtn.addEventListener('click', function() {
